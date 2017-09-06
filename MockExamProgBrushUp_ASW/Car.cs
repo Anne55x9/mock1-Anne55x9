c6e05ab9aef8fe38efccdfc1ba3a9268b87a489a
Anne Sofie Molsted Wanscher <anne55x9@edu.easj.dk>
2017-09-06 09:29:58 +0200

    Starten et class libraru (.NET framework) som implementerer klasserne Car og Owner. I klassen car er tilføjet 4 full props med private instans felter og public metode del.

---
+++ MockExamProgBrushUp_ASW/Car.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MockExamProgBrushUp_ASW
+{
+    public class Car
+    {
+
+        #region full properties til klassen Car med privat instance felter og public metode.
+
+        private Enum color;
+
+        public Enum Color
+        {
+            get { return color; }
+            set { color = value; }
+        }
+
+        private int doors;
+
+        public int Doors
+        {
+            get { return doors; }
+            set { doors = value; }
+        }
+
+        private String model;
+
+        public String Model
+        {
+            get { return model; }
+            set { model = value; }
+        }
+
+        private String registrationNo;
+
+        public String RegistrationNo
+        {
+            get { return registrationNo; }
+            set { registrationNo = value; }
+        }
+
+        #endregion
+        public Car()
+        {
+
+        }
+    }
+}
--- MockExamProgBrushUp_ASW/Owner.cs
+++ MockExamProgBrushUp_ASW/Owner.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace MockExamProgBrushUp_ASW
 {
-    public class Class1
+    class Owner
     {
     }
 }
